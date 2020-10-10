660f5f8f88749140943d9d2a141f82f9194ad2fc
Travis Stevenson <thetechtravis@outlook.com>
2020-10-10 14:33:07 -0500

    Added Javascript objects to represent journal entries for some of the things I have learned so far at NSS in JournalDataProvider.js. Modified label for entry-text in index.html.

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -1,1 +1,61 @@
-// This module is responsible for managing the state of the application
+/* This module is responsible for managing the state of the application
+ *
+ *      Holds the raw data about each entry and exports
+ *      functions that other modules can use to filter
+ *      the entries for different purposes.
+ */
+
+// This is the original data.
+const journalEntries = [
+    {
+        id: 1,
+        'first-name': "Travis",
+        'last-name': "Stevenson",
+        date: "07/24/2020",
+        concept: "HTML & CSS",
+        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
+        mood: "Ecstatic"
+    },
+
+    {
+        id: 2,
+        'first-name': "Travis",
+        'last-name': "Stevenson",
+        date: "07/25/2020",
+        concept: "Flexbox",
+        entry: "We talked about CSS Flexbox and how to use it to style your webpage responsively.",
+        mood: "Happy"
+    },
+
+    {
+        id: 3,
+        'first-name': "Travis",
+        'last-name': "Stevenson",
+        date: "07/26/2020",
+        concept: "Complex Flexbox",
+        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Scott.",
+        mood: "Content"
+    },
+
+    {
+        id: 4,
+        'first-name': "Travis",
+        'last-name': "Stevenson",
+        date: "07/27/2020",
+        concept: "Iteration",
+        entry: "We talked about for loops.",
+        mood: "Sad"
+    }
+]
+
+/*
+    You export a function that provides a version of the
+    raw data in the format that you want
+*/
+export const useJournalEntries = () => {
+    const sortedByDate = journal.sort(
+        (currentEntry, nextEntry) =>
+            Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+    )
+    return sortedByDate
+}
