ab15751e6b0e1eb7cdb78c4feb9b2717aca5bad7
Travis Stevenson <thetechtravis@outlook.com>
2020-10-12 11:56:21 -0500

    Removed placeholder text from JournalEntry.js

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -5,12 +5,9 @@
 export const JournalEntryComponent = (entryObj) => {
     return `
         <section id="entryObj--${entryObj.id}" class="journalEntry">
-            Display the entryObj's full text, and the date
-            it was entered here.
-
             <h2>${entryObj.concept}</h2>
             <p>${entryObj.entry}</p>
-            <p>${entryObj["first-name"]} ${entryObj["last-name"]}</p>
+            <p>- ${entryObj["first-name"]} ${entryObj["last-name"]}</p>
             <p>${entryObj.date}</p>
         </section>
     `
